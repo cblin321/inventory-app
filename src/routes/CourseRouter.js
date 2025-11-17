2dc21ce5ff5f55456aa0aefd25973b4fd07ce16b
Chris Lin <chrislin90210@gmail.com>
2025-11-16 20:22:35 -0600

    courses

--- src/routes/CourseRouter.js
+++ src/routes/CourseRouter.js
@@ -1,40 +1,38 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const courseRouter = Router()
-const coursesController = require("../controllers/CoursesController")
+const courseRouter = Router();
+const coursesController = require("../controllers/CoursesController");
+const offeringsController = require("../controllers/OfferingsController");
 
 courseRouter.get("/", async (req, res) => {
-        const allCourses = await coursesController.getAllCourses()
-        res.render("./courses/courses", { courses: allCourses })
-    }
-)
-
+  let allCourses = await coursesController.getAllCourses();
+  allCourses = allCourses.map((course) => ({
+    ...course,
+    offeringsURL: `../offerings/${course.course_number}`,
+  }));
+  res.render("./courses/courses", { courses: allCourses,
+    addURL: "/courses/add/"
+  });
+});
 
 courseRouter.get("/:id", async (req, res) => {
-    const params = req.params
+  const course = await coursesController.getOne(req, res);
+  const offerings = await offeringsController.getAllForCourse(req, res);
+  res.render("./courses/course", { offerings, course });
+});
 
-    const course = await coursesController.getOne(parseInt(params.id))
-    const offerings = await coursesController.getOfferings(result[0]) 
-    res.render("./course", { offerings, course })
-})
-
-courseRouter.get("/course/:id/subjects", async (req, res) => {
-
-})
+courseRouter.get("/course/:id/subjects", async (req, res) => {});
 
 courseRouter.delete("/course/:id", async (req, res) => {
-    coursesController.deleteCourse(req, res)
-
-})
+  coursesController.deleteCourse(req, res);
+});
 
 courseRouter.post("/course/add", async (req, res) => {
-    coursesController.addCourse(req, res)
-
-})
+  coursesController.addCourse(req, res);
+});
 
 courseRouter.put("/course/:id", async (req, res) => {
-    coursesController.updateCourse(req, res)
-})
+  coursesController.updateCourse(req, res);
+});
 
-
-module.exports = courseRouter
+module.exports = courseRouter;
